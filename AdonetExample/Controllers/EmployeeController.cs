f8e90dd71876f1159aa32784b8919979fe8680b9
yawar2019 <yawarali17@gmail.com>
2020-10-13 08:10:33 +0530

    updated Code

---
+++ AdonetExample/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using AdonetExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdonetExample.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            
+            return View(db.GetAllEmployees());
+        }
+
+        [HttpGet]
+        public ActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee obj)
+        {
+            return View();
+        }
+    }
+}
---
+++ AdonetExample/Models/EmployeeContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace AdonetExample.Models
+{
+    public class EmployeeContext
+    {
+        SqlConnection con = new SqlConnection("Data Source=AZAM-PC\\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=true");
+
+        public List<Employee> GetAllEmployees() {
+            List<Employee> listobj = new List<Employee>();
+            SqlCommand cmd = new SqlCommand("spr_getEmployeeDetails",con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                Employee obj = new Employee();
+                obj.EmpId = Convert.ToInt32(dr[0]);
+                obj.EmpName = Convert.ToString(dr[1]);
+                obj.EmpSalary = Convert.ToInt32(dr[2]);
+                listobj.Add(obj);
+            }
+            return listobj;
+        }
+
+    }
+}
